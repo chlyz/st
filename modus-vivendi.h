71b31cf94c7c49d40dfdeef732eb6ff3baff298a
Christian Lyzell <christian.lyzell@gmail.com>
2022-04-03 21:37:21 +0200

    config: add modus operandi/vivendi colors

--- modus-vivendi.h
+++ modus-vivendi.h
@@ -1,32 +1,32 @@
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#ffffff", // bg_main
-	"#a60000", // red
-	"#005e00", // green
-	"#813e00", // yellow
-	"#0031a9", // blue
-	"#721045", // magenta
-	"#00538b", // cyan
-	"#000000", // fg_main
+	"#000000", // bg_main
+	"#ff8059", // red
+	"#44bc44", // green
+	"#d0bc00", // yellow
+	"#2fafff", // blue
+	"#feacd0", // magenta
+	"#00d3d0", // cyan
+	"#ffffff", // fg_main
 
 	/* 8 bright colors */
-	"#f2eff3", // bg_hl_line
+	"#505050", // bg_alt
 	"#a60000", // red_alt_other
         "#005e00", // green_alt_other
 	"#863927", // yellow_alt_other
 	"#0000c0", // blue_alt_other
 	"#5317ac", // magenta_alt_other
 	"#005a5f", // cyan_alt_other
-	"#505050", // fg_alt
+	"#f2eff3", // bg_hl_line
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#000000",
 	"#ffffff",
-	"#000000", /* default foreground colour */
-	"#ffffff", /* default background colour */
+	"#000000",
+	"#ffffff", /* default foreground colour */
+	"#000000", /* default background colour */
 };
 
 /*
